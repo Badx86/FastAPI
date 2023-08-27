e53f576eeff3a8252ced2d8f1d0e09a4cc338b06
badx86 <badlolpro@gmail.com>
2023-08-27 17:19:36 +0400

    add GET, PUT, DELETE methods

--- Project 1/books.py
+++ Project 1/books.py
@@ -34,6 +34,17 @@ async def read_category_by_query(category: str):
     return books_to_return
 
 
+# Get all books from a specific author using path or query parameters
+@app.get("/books/by_author/{author}")
+async def read_books_by_author_path(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
 @app.get("/books/{book_author}/")
 async def read_author_category_by_query(book_author: str, category: str):
     books_to_return = []
@@ -48,3 +59,18 @@ async def read_author_category_by_query(book_author: str, category: str):
 @app.post("/books/create_book")
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break

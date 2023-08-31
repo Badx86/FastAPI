25b646832f116ce291f6912420525ac9eb4c7c0b
badx86 <badlolpro@gmail.com>
2023-08-31 04:11:47 +0400

    Authentication Enhancements

--- Project_3/TodoApp/routers/todos.py
+++ Project_3/TodoApp/routers/todos.py
@@ -7,7 +7,10 @@ from models import Todos
 from database import SessionLocal
 
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/todo',
+    tags=['ToDo']
+)
 
 
 def get_db():
@@ -33,7 +36,7 @@ async def read_all(db: db_dependency):
     return db.query(Todos).all()
 
 
-@router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+@router.get("/{todo_id}", status_code=status.HTTP_200_OK)
 async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo_model is not None:
@@ -41,7 +44,7 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     raise HTTPException(status_code=404, detail='Todo not found.')
 
 
-@router.post("/todo", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_todo(db: db_dependency, todo_request: TodoRequest):
     todo_model = Todos(**todo_request.dict())
 
@@ -49,7 +52,7 @@ async def create_todo(db: db_dependency, todo_request: TodoRequest):
     db.commit()
 
 
-@router.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.put("/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_todo(db: db_dependency,
                       todo_request: TodoRequest,
                       todo_id: int = Path(gt=0)):
@@ -66,7 +69,7 @@ async def update_todo(db: db_dependency,
     db.commit()
 
 
-@router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo_model is None:
